02af16794bd32ba000cde46ac5618e10a1c1d3a6
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-02-02 16:20:43 +0200

    Add files via upload

--- API/Infrastructure/RequestDTOs/StudentProfessors/StudentProfessorsFilterRequest.cs
+++ API/Infrastructure/RequestDTOs/StudentProfessors/StudentProfessorsFilterRequest.cs
@@ -1,1 +1,9 @@
+using System;
 
+namespace API.Infrastructure.RequestDTOs.StudentProfessors;
+
+public class StudentProfessorsFilterRequest
+{
+    public int StudentID { get; set; }
+    public int ProfessorID { get; set; }
+}
