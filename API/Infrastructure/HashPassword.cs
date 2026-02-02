a5fc430e164ac037446f1d700da7cbcf34170525
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-02-02 16:07:39 +0200

    Add files via upload

--- API/Infrastructure/HashPassword.cs
+++ API/Infrastructure/HashPassword.cs
@@ -1,1 +1,14 @@
+using System;
+using BCrypt.Net;
 
+namespace API.Infrastructure;
+
+public class HashPassword
+{
+    public static string Hash(string password)
+    {
+        string hash = BCrypt.Net.BCrypt.HashPassword(password, workFactor: 13);
+
+        return hash;
+    }
+}
