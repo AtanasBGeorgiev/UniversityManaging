1e7f1dc86dc3baa276de0dc5779cd4402704a388
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-02-02 15:40:30 +0200

    Add files via upload

--- Common/Services/BaseService.cs
+++ Common/Services/BaseService.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using Common.Entities;
 using Common.Persistance;
 using Microsoft.EntityFrameworkCore;
 
 namespace Common.Services;
 
 public class BaseService<T>
-where T : class
+where T : BaseEntity
 {
     private DbContext Context { get; set; }
     private DbSet<T> Items { get; set; }
@@ -28,7 +29,7 @@ where T : class
         if (!string.IsNullOrEmpty(orderBy))
         {
             if (sortAsc)
-                query = query.OrderBy(e => EF.Property<object>(e, orderBy));
+                query = query.OrderBy(e => EF.Property<object>(e, orderBy));//takes the property value
             else
                 query = query.OrderByDescending(e => EF.Property<object>(e, orderBy));
         }
@@ -38,6 +39,16 @@ where T : class
         .Take(pageSize);
 
         return query.ToList();
+    }
+    public T GetByProperty(Expression<Func<T, bool>> filter)
+    {
+        var query = Items.AsQueryable();
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+
+        return query.FirstOrDefault();
     }
 
     public int Count(Expression<Func<T, bool>> filter = null)
@@ -50,29 +61,48 @@ where T : class
 
         return query.Count();
     }
-    public T GetById(int id)
+    public T GetById(int id, int secondId = 0)
     {
-        return Items.Find(id);
-    }
-    public void Save(T item)
-    {
-        var keyName = Context.Model.FindEntityType(typeof(T)) //Context.Model contains metadata for every entity(table)
-        .FindPrimaryKey()
-        .Properties           //takes all attributes participating in the primary key
-        .Select(p => p.Name)  //reads theirs names
-        .First();
-
-        var keyValue = typeof(T).GetProperty(keyName).GetValue(item); //read the value of the attribute
-
-        int id = keyValue != null ? (int)keyValue : 0;
-
-        if (id > 0)
-            Items.Update(item);
+        if (secondId == 0)
+            return Items.Find(id);
         else
+            return Items.Find(id, secondId);
+    }
+    public void Save(T item, int oldSecondId = 0, bool update = false)
+    {
+        int[] PK = item.GetIds();
+
+        if (PK.Length == 1 && PK[0] == 0)//if has one attribute in PK and item is new
+        {
             Items.Add(item);
+        }
+        else if (PK.Length == 1 && PK[0] != 0)//if has one attribute in PK and item is for update
+        {
+            Items.Update(item);
+        }
+        else if (PK.Length == 2)//if has two attributes in PK
+        {
+            BaseService<T> composite = new BaseService<T>();
+
+            if (oldSecondId != 0)//if has update in the composite key
+            {
+                var oldItem = composite.GetById(PK[0], oldSecondId);
+                if (oldItem != null)//item contains new second id
+                {
+                    //EF does not allow replacement in a composite PK
+                    Items.Remove(oldItem);
+                    Items.Add(item);
+                }
+            }
+            else if (update == true)//update out of composite key
+                Items.Update(item);
+            else
+                Items.Add(item);
+        }
 
         Context.SaveChanges();
     }
+
     public void Delete(T item)
     {
         Items.Remove(item);
