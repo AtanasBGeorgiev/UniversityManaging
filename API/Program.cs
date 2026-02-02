ad7829bff8644d7f21db481cd9f19cea4ccf0cbd
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-02-02 15:58:28 +0200

    Add files via upload

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,64 @@
+using System.Text;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using DotNetEnv;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using Common.Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+Env.Load();
+var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET");
+var key = Encoding.ASCII.GetBytes(jwtSecret);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),//checks original and used secret key
+            ValidIssuer = "ag",
+            ValidAudience = "front-end",
+            RoleClaimType = "role"
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", p => p.RequireClaim(ClaimTypes.Role, "1"));
+    options.AddPolicy("Professor", p => p.RequireClaim(ClaimTypes.Role, "2"));
+    options.AddPolicy("Student", p => p.RequireClaim(ClaimTypes.Role, "3"));
+    options.AddPolicy("AdminOrProfessor", p => p.RequireClaim(ClaimTypes.Role, "1", "2"));
+    options.AddPolicy("AdminOrStudent", p => p.RequireClaim(ClaimTypes.Role, "1", "3"));
+});
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+/*builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});*/
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+//app.UseCors("AllowFrontend");
 
 app.Run();
