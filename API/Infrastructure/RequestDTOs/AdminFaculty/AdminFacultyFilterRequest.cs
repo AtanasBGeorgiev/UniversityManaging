83557eb90502f924669ac50c642b55c2fda0fab4
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-02-02 16:12:24 +0200

    Add files via upload

--- API/Infrastructure/RequestDTOs/AdminFaculty/AdminFacultyFilterRequest.cs
+++ API/Infrastructure/RequestDTOs/AdminFaculty/AdminFacultyFilterRequest.cs
@@ -1,1 +1,9 @@
+using System;
 
+namespace API.Infrastructure.RequestDTOs.AdminFaculty;
+
+public class AdminFacultyFilterRequest
+{
+    public int AdminID { get; set; }
+    public int FacultyID { get; set; }
+}
