4f3b1ced63132a30a47d9b52893b231cba2c3a30
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-02-02 16:14:55 +0200

    Add files via upload

---
+++ API/Infrastructure/RequestDTOs/CourseProfessors/CourseProfessorsGetRequest.cs
@@ -0,0 +1,9 @@
+using System;
+using API.Infrastructure.RequestDTOs.Shared;
+
+namespace API.Infrastructure.RequestDTOs.CourseProfessors;
+
+public class CourseProfessorsGetRequest : BaseGetRequest<CourseProfessorsFilterRequest>
+{
+
+}
